2e6fae1b28fb7f98ff8135fc13e96af8cb512d44
yanlu <yanlu@student.42berlin.de>
2026-08-07 19:20:07 +0200

    update gamemanager

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -2,14 +2,16 @@ extends Node
 
 var score: int = 0
 var player_health: int = 10
-var message = "You won!"
+var message = "You lost!"
+
+func reset_game_data():
+	score = 0
+	player_health = 10
+	message = "You lost!"
 
 # Score
 func add_score(points: int = 1):
 	score += points
-
-func reset_score():
-	score = 0
 
 # Health
 func lose_health(points: int = 1):
@@ -17,3 +19,7 @@ func lose_health(points: int = 1):
 
 func gain_health(points: int = 1):
 	player_health += points
+
+# Update end message
+func win_message():
+	message = "You won!"
