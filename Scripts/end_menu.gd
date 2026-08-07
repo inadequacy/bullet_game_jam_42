c8e8627f4f946eef5380867a8d1c15589e6a7866
yanlu <yanlu@student.42berlin.de>
2026-08-07 17:51:04 +0200

    add end menu

---
+++ Scripts/end_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Level/level.tscn")
+
+
+func _on_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/start_menu.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
