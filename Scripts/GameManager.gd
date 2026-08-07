ed9cea4a9be0827336976cb5924412dc734a3b2b
yanlu <yanlu@student.42berlin.de>
2026-08-07 19:10:55 +0200

    add score to main scene

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,10 +1,19 @@
 extends Node
 
 var score: int = 0
+var player_health: int = 10
 var message = "You won!"
 
+# Score
 func add_score(points: int = 1):
 	score += points
 
 func reset_score():
 	score = 0
+
+# Health
+func lose_health(points: int = 1):
+	player_health -= points
+
+func gain_health(points: int = 1):
+	player_health += points
