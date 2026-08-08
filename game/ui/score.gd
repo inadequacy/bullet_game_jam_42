2953d86c3f498c6baa977a77459dbe5bf373c792
yanlu <yanlu@student.42berlin.de>
2026-08-08 12:51:20 +0200

    increase score when enemy killed

--- game/ui/score.gd
+++ game/ui/score.gd
@@ -1,5 +1,8 @@
 extends Label
 
-
 func _ready() -> void:
-	$".".text = "Score: " + str(GameManager.score)
+	text = "Score: " + str(GameManager.score)
+	GameManager.score_changed.connect(_on_score_changed)
+
+func _on_score_changed(new_score: int) -> void:
+	text = "Score: " + str(new_score)
